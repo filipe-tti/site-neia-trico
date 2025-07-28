9c589d94414268c6588dcab7931f6d5f9a53b9d6
Filipe Silva <52262356+filipe-tti@users.noreply.github.com>
2025-07-27 21:09:03 -0300

    configurações deploy

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const repo = 'site-neia-trico';
+
+module.exports = {
+  output: 'export',
+  basePath: repo,
+  assetPrefix: repo + '/',
+};
+
 export default nextConfig;
